001b35e6c304cd6b53c1233099f4bb4b541c3c17
Mika-Cuvillier <73265072+Mika-Cuvillier@users.noreply.github.com>
2021-03-15 13:01:24 -0400

    fin de use state et lazy initialization

--- src/ConteneurGlobal.jsx
+++ src/ConteneurGlobal.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './ConteneurGlobal.scss';
 import Entete from './Entete';
 import Accueil from './Accueil';
@@ -9,19 +9,19 @@ import PiedDePage from './PiedDePage';
 import { Switch, Route } from 'react-router-dom';
 
 export default function ConteneurGlobal() {
-  
-  // Vérifier s'ily a quelque chose dans local storage avec la clé 'panier-4pa
-  let panier = {};
-  if(window.localStorage.getItem('panier-4pa')){
-    panier = JSON.parse(window.localStorage.getItem('panier-4pa'));
-  }
-
+ 
   //utiliser la gestion de l'état avec "useState"
-  const etatPanier = useState(panier);
+  const etatPanier = useState(
+    () => window.localStorage.getItem('panier-4pa') ? JSON.parse(window.localStorage.getItem('panier-4pa')) : {}
+  );
+  const [panier] = etatPanier;
 
-  // Sauvegarder le panier dans localStorage à chaques fois que l'état du panier est muté.
-  window.localStorage.setItem('panier-4pa', JSON.stringify(etatPanier[0]));
-
+  // Sauvegarder le panier dans localStorage à chaque fois que l'état du panier est muté.
+  useEffect(
+    () => window.localStorage.setItem('panier-4pa', JSON.stringify(panier))
+    , [panier]
+  );
+  
   return (
     <div className="ConteneurGlobal">
       <Entete etatPanier={etatPanier} />
--- src/Produit.jsx
+++ src/Produit.jsx
@@ -1,15 +1,16 @@
 import BtnAjoutPanier from './BtnAjoutPanier';
 import './Produit.scss';
 
-export default function Produit(props){
-    const [panier, setPanier] = props.etatPanier;
+export default function Produit({id, nom, etatPanier, prix}){
+
+    const [panier, setPanier] = etatPanier;
 
     function ajouterAuPanier(){
-        const article = panier[props.id];
+        const article = panier[id];
         if(article){
             article.qte += 1; 
         }else{
-            panier[props.id] = {prix: props.prix, qte: 1};
+            panier[id] = {prix: prix, qte: 1};
         }
         
         console.log(panier);
@@ -19,17 +20,17 @@ export default function Produit(props){
     let qteArticlePanier = 0;
     let texteBtnAjout = "Ajouter au panier";
     let btnCouleurAjout ="";
-    if(panier[props.id]){
-        qteArticlePanier = panier[props.id].qte;
+    if(panier[id]){
+        qteArticlePanier = panier[id].qte;
         texteBtnAjout = "Augmenter la quantité"
         btnCouleurAjout = "#900";
     }
     return(
         <li className="Produit"> 
-            <img src={'images-produits/' + props.id + '.webp'} alt={props.nom} />
+            <img src={'images-produits/' + id + '.webp'} alt={nom} />
             <div className="info">
-                <p className="nom">{props.nom}</p>
-                <p className="prix">{props.prix} $CA</p>
+                <p className="nom">{nom}</p>
+                <p className="prix">{prix} $CA</p>
             </div>
             <BtnAjoutPanier onClick={ajouterAuPanier} qte={qteArticlePanier} texte={texteBtnAjout} couleur={btnCouleurAjout} />
         </li>
